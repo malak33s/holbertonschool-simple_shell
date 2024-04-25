8fd63425324521b65a61213c50acd6e233e92bd8
malak33s <melaksarhani1bga@gmail.com>
2024-04-25 10:19:51 +0000

    change for check 1 add getline and fprintf

--- shell.c
+++ shell.c
@@ -9,28 +9,32 @@ int procommand(char *command)
 	char *args[2];
 	char *token = strtok(command, " \n");
 	pid_t pid = fork();
-	args[0] = token;
-	args[1] = NULL;
+
 	
+
 	if (pid < 0)
 	{
-		perror("fork");
+		fprintf(stderr, "error \n");
 		return -1;
 	}
 	else if (pid == 0)
 	{
+		args[0] = token;
+		args[1] = NULL;
+		
 		if (execve(args[0], args, NULL) == -1)
 		{
-			perror("./hsh");
+			fprintf(stderr, "Error: execve failed\n");
 			exit(EXIT_FAILURE);
 		}
 	}
 	else
 	{
 		int status;
+
 		if (waitpid(pid, &status, 0) == -1)
 		{
-			perror("waitpid");
+			fprintf(stderr, "Error: waitpid failed\n");
 			return -1;
 		}
 	}
